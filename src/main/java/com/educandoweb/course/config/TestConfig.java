06cc4d2f710f12622c21b770e93d0975bdec835b
Maykon Ferreira <lmaykonf@gmail.com>
2020-07-30 13:32:45 -0300

    Order, Instant, ISO 8601 PARTE 2

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositores.OrderRepository;
 import com.educandoweb.course.repositores.UserRepository;
 
 @Configuration
@@ -17,7 +20,12 @@ public class TestConfig implements CommandLineRunner {
 	// Isso resolve a dependencia (Spring Tool Suite)
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// tudo funciona quando instaciar a aplicação
@@ -25,7 +33,15 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Maykon Ferreira", "maykon@gmail.com", "977777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o4 = new Order(null, Instant.parse("2020-07-30T15:21:22Z"), u3);
+		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3,o4));
+		
+		
 		
 	}
 	

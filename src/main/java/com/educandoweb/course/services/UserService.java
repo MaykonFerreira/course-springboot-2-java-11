c78e36bb2635d856db94c0d038f667f500231278
Maykon Ferreira <lmaykonf@gmail.com>
2020-07-31 20:00:44 -0300

    Exception handling - Update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -18,53 +20,56 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
-		
+
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
-		
+
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		
-		// tenta o get e não tiver 
+
+		// tenta o get e não tiver
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-		
+
 	}
+
 	public void delete(Long id) {
 		try {
-			repository.deleteById(id);			
-		}
-		catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException(id); 
-		}
-		catch(DataIntegrityViolationException e) {
-			//e.printStackTrace();
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			// e.printStackTrace();
 			throw new DatabaseException(e.getMessage());
 		}
 
-		
 	}
-	
-	public User update(Long id,User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity,obj);
-		return repository.save(entity);
-		
-		
+
+	public User update(Long id, User obj) {
+
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}		
+
+
 	}
 
 	private void updateData(User entity, User obj) {
-		
+
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setFone(obj.getFone());
-		
-		
+
 	}
 }
